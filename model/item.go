6af4bdb8640e4038a526d425faa4f4a1819ff060
Bangkit Anom Sedhayu <bangkitunom87@gmail.com>
2024-05-15 20:46:02 +0700

    CREATE - all model users,items,categories,activities,sessions

---
+++ model/item.go
@@ -0,0 +1,11 @@
+package model
+
+import "gorm.io/gorm"
+
+type Items struct {
+	gorm.Model
+	CategoryID  int     `gorm:"column:category_id;not null" json:"category_id"`
+	Quantity    int     `gorm:"column:quantity;not null" json:"quantity"`
+	Price       float64 `gorm:"column:price;not null" json:"price"`
+	Description string  `gorm:"column:description;type:text" json:"description"`
+}
